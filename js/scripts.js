3510f079d0bee14ba5471b50e8ba2d3784dddeda
karenbala <karenbala@gmail.com>
2022-01-11 21:56:40 -0500

    Update scripts.js
    added new javascript code for menuToggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("Hello! And Welcome to my portfolio!")
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+}
